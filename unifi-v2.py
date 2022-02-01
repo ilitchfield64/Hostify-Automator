069d2c13207835eb1d9894ba0d3f241f3e138e2e
Ian Litchfield <ilitchfield64@gmail.com>
2022-02-01 10:48:05 -0500

    Testing creating a list csv from the ping to grab info on subnet then only attack those in the list

---
+++ unifi-v2.py
@@ -0,0 +1,214 @@
+## This program is a short script to blast a whole network to join
+## any and all Unifi devices to the cloud management system
+## Use with caution on New Unifi devices only
+## IN theory it should only affect the Unifi devices, however a firewall
+## may block traffic due to excesive pings
+## Anything in between the '### ~~~~~ ###' are meant to be user editable variables to adjust the script to your needs
+
+
+
+### Imports
+from sys import exit
+from sys import argv
+from os import system, name
+import platform
+import subprocess
+import paramiko
+
+
+### Declartaions
+
+
+
+## CHANGE THIS TO SET THE UNIFY LINK ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #####
+
+unifi_link = 'http://unifi.tech-keys.com:8080/inform'
+
+username = "ubnt"
+password = "ubnt"
+
+#### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #####
+
+ip_string = 0
+pass_list = []
+fail_list = []
+#AP_pass_count = 0
+
+
+ip_log = open("ip_log.txt", "w")
+
+
+
+
+e_log = open("ErrorLog.txt","w")
+e_log.write("Starting Error Logging...\n")
+c_log = open("ConsoleLog.txt","w")
+c_log.write("Starting Console Output Log...\n")
+
+### Functions
+
+## Converts IP back to string for use elsewhere
+def list_to_str(list1):
+    string1 = str(list1[0]) + "." + str(list1[1]) + "." + str(list1[2]) + "." + str(list1[3])
+    return string1
+
+## Pings the remote host to determine if the AP is up
+def pinger(host):
+
+    # return True # this is for debugging REMOVE ME
+    parameter = '-n' if platform.system().lower()=='windows' else '-c'
+
+    command = ['ping', parameter, '1', host]
+    response = subprocess.call(command)
+
+    if response == 0:
+        print("Ping!")
+
+        return True
+    else:
+        print("Failed to Ping")
+        return False
+
+## This executes an SSH Sesh and runs a remote command MAY NEED TO ADD EXCEPTIONS TO THIS METHOD NEEDS TESTING
+def AP_info_setter(user_name, pass_word, host, link):
+   ## Creates the SSH Session
+    c_log.write("Opening SSH on " + str(host))
+    port = 22
+    command = "mca-cli-op set-inform " + str(link) # This '/inform/ might not be needed if the link has it already
+    #command = 'help'
+    print(command)
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(host, port, user_name, pass_word)
+
+    ## Runs remote command and prints the output
+    stdin, stdout, stderr = ssh.exec_command(str(command))
+    print(stdin)
+    print(stdout)
+    print(stderr)
+    c_log.write(str(stdout) +'\n'+ str(stderr) + '\n')
+    for line in stdout:
+        print(line)
+        c_log.write(str(line) + "\n")
+    ## Closes the  SSH Sesh and prints the std terminal
+    ssh.close()
+    print(stdin)
+    print(stdout)
+    print(stderr)
+    c_log.write(str(stderr) +'\n'+ str(stderr) + '\n')
+    c_log.write(str(ip_string))
+    
+    # Need to lint the 'lines' variable to see if it says failure' 
+
+##  Runs when any step fails and will output the failure to a log file in the PWD
+def failure(ip_address):
+    print("Fail")
+    #fail_list.append(list_to_str(ip_address))
+    #print(fail_list)
+    e_log.write(ip_address + ": Failed\n")
+
+#==================================================================================================================================#
+
+### Default Loop 
+def default(username,password):
+
+
+### Input data for script 
+    print()
+    print()
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    ip_string = input("Enter the Network Subnet for the Unifi Network: ")           #### COMMENT OUT THE 'INPUT' PART OF THE LINE AND SET THE STATIC SUBNET
+  
+    host = input("Enter the Stating host of the subnet: ")                          #### COMMENT OUT THE 'INPUT' PART OF THE LINE AND SET THE STATIC STARTING HOST
+    
+    username = username     # "ubnt"       #= input("Enter the Unifi Account: ")    #### UNCOMMENT AND CHANGE TO EQUAL TO INPUT TO SET AT RUNTIME
+    password = password     # "ubnt"       #= input("Enter the Unifi Pasword: ")    #### UNCOMMENT AND CHANGE TO EQUAL TO INPUT TO SET AT RUNTIME
+
+    ##unifi_link = input("Enter the Unifi Cloud Link: ")                            #### UNCOMENT TO SET THE INFORM LINK AT RUNTIME
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###  
+
+### Takes IP and breaks it up into a list
+    ip_address = ip_string.split('.')                                               
+    ip_address[3] = int(ip_address[3])                                              
+    ip_address[3] = int(host)                                                       
+    ip_string = list_to_str(ip_address)
+
+
+
+
+
+# Some debug code for testing
+    
+#    print(ip_address[3])
+   
+#    print(ip_string)
+
+#    pinger(ip_string)
+
+
+### Primary loop
+
+    ## The code will run from address in range x thru 254
+    try:
+        for ip_address[3] in range(100, 255):
+            ip_string = list_to_str(ip_address)
+        
+            ## Tries to ping a remote host, and then attempts to open an SSH Session on success
+            if pinger(list_to_str(ip_address)):
+                print("Pass")
+                ## Ignore this comment Fixing issues                                                               ## The following method fails to a hard exit, need to add error exceptions if the command fails and to log the IP missed
+                #Try this
+                try:
+                    AP_info_setter(username, password, list_to_str(ip_address), unifi_link) 
+                    pass_list.append(list_to_str(ip_address))
+                    ## Increment the counter up for passes
+                    #global AP_pass_count += 1
+                
+                    # On try fail run failure() method. THE ERROR EXCEPTION FOR FAILING AN SSH SESSION MAY NEED TO BE IN THE AP_info_setter() METHOD
+                except EOFError as e:
+                    print(e)
+                    failure(list_to_str(ip_address))
+                    
+                
+                except paramiko.ssh_exception.AuthenticationException as e:  ## This excepts the Autentication error from SSH upon failed login
+                    
+                    print(e)
+                    failure(list_to_str(ip_address))
+                
+                except paramiko.ssh_exception.NoValidConnectionsError as e:  ## This excepts a general failure in SSH ie: falied to connect to IP
+                    print(e)
+                    failure(list_to_str(ip_address)) 
+                except TimeoutError as e:
+                    print(e)
+                    failure(list_to_str(ip_address))
+
+
+            else:
+                failure(list_to_str(ip_address))
+            
+    except KeyboardInterrupt as e:
+        print("Intrrupted by User")
+        e_log.write("Interupted by user\n" +str(e))
+        c_log.write("Interupted by User\n")
+
+        
+    ## Shows the number of APs completed 
+    #print("The numeber of APs passed is: " + AP_pass_count)
+    print("Check Log file for any errors")
+    e_log.close()
+    c_log.close()
+
+def read_csv(file_name):
+
+    pass
+    
+
+
+
+default(username,password)
+
+
+
+# There is a difference in how the windows version of ping handles the error of a failed ping vs *nix 
+# All pings are counting in windows as a "Success" in my code, but an error exception in the SSH attempt 
+# Keeps the code moving. Will need to update accordingly... or implement a way to read and use a CSV
